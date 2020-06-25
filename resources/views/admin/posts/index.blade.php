b74b41fa8f7523a1a7b16e31d06ae4eb507f6b46
Luigi <luigismilzo@live.com>
2020-06-25 17:11:51 +0200

    Added admin layout, index page and resource routes

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="container">
+    <h1 class="mb-4">Archive</h1>
+
+    @foreach($posts as $post)
+        <article>
+            <h2>{{ $post->title }}</h2>
+            <p>{{ $post->body }}</p>
+            @if (! $loop->last)
+                <hr>
+            @endif
+        </article>
+    @endforeach
+
+    <div class="pagination mt-5 d-flex justify-content-center">
+        {{ $posts->links() }}
+    </div>
+</div>
+@endsection
