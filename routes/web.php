e9563827bb5195ad5c34636098396a94177e9ad4
Luigi <luigismilzo@live.com>
2020-06-25 16:31:04 +0200

    Scaffolding bootstrap-ui, auth, guest index

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('guest.welcome');
+})->name('home');
+
+Auth::routes();
+
+//Guest routes
+Route::get('posts', 'PostController@index')->name('posts.index');
+
+// Admin routes
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware('auth')
+    ->group(function() {
+        Route::get('/home', 'HomeController@index')->name('admin.home');
+    });
